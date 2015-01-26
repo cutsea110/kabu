21ab800dd6482e5dd49b9501deaba045ae81ff5b
cutsea110 <cutsea110@gmail.com>
2015-01-26 18:49:38 +0900

    Alter table rename stocks to stock

--- createtable.sql
+++ createtable.sql
@@ -1,4 +1,4 @@
-create table stocks (
+create table stock (
   day date not null,
   code varchar(16) not null,
   name varchar(128) not null,
@@ -13,9 +13,9 @@ create table stocks (
   PRIMARY KEY (day, code, market)
 );
 
-create index idx_day on stocks (day);
-create index idx_code on stocks (code);
-create index idx_name on stocks (name);
-create index idx_market on stocks (market);
-create index idx_category on stocks (category);
+create index idx_stock_day on stocks (day);
+create index idx_stock_code on stocks (code);
+create index idx_stock_name on stocks (name);
+create index idx_stock_market on stocks (market);
+create index idx_stock_category on stocks (category);
 
